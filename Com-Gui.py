9df20e0d534dc52b2a596e641711c3f05c71021a
Unimatrix00 <ak860916@gmail.com>
2016-07-26 11:06:16 +0100

    Update Com-Gui.py
    - part of this must be merged with V2.py
    - Reason : enable line by line reading and text.see(END) take he user to the button of the list after every iteration

--- Com-Gui.py
+++ Com-Gui.py
@@ -1,9 +1,11 @@
 def get_likes_button():
     child = subprocess.Popen(['python', '-u', 'getLikes.py'], stdout=subprocess.PIPE)
     text.delete("1.0",END)
+    #--- This part must be placed into V2.py ---
     for line in iter(child.stdout.readline, ''):
         text.insert(INSERT, line)
         text.see(END)
         text.update_idletasks()
+    #--------------END---------------------
     child.stdout.close()
     child.wait()
